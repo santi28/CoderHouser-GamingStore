9cc5b453d0d08dec0ffd090097b069423dce65f9
Santiago de Nicolás <santidenicolas@gmail.com>
2022-08-27 16:54:13 -0300

    :sparkles: Added header with styles

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import '../css/styles.css'
+
+function NavBar() {
+  return(
+    <header className='flex items-center justify-between p-4 px-6 bg-slate-900'>
+      <a href="/">
+        <img className='h-16' src="/images/TGSIcon.png" alt="The Gaming Store Icon" />
+      </a>
+      <nav>
+        <ul className='flex gap-4 font-bold text-white text-md'>
+          <li className='p-2 px-4 rounded-full cursor-pointer hover:bg-slate-100 hover:text-slate-900'><a href="/">Inicio</a></li>
+          <li className='p-2 px-4 rounded-full cursor-pointer hover:bg-slate-100 hover:text-slate-900'><a href="/">Juegos</a></li>
+          <li className='p-2 px-4 rounded-full cursor-pointer hover:bg-slate-100 hover:text-slate-900'><a href="/">Consolas</a></li>
+          <li className='p-2 px-4 rounded-full cursor-pointer hover:bg-slate-100 hover:text-slate-900'><a href="/">Tutoriales</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
