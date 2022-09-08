e82aa3fd75dbcf3a71717ea42edc6b3b2b87d597
Santi de Nicolás <santidenicolas@gmail.com>
2022-09-08 22:37:55 +0100

    añadido card de producto

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,17 @@
+import Item from './Item';
+
 function ItemListContainer({ greeting }) {
   return (
-    <h1 className="py-4 text-2xl font-bold text-center">{ greeting || 'Aqui va el catalogo' }</h1>
+    <>
+      <Item 
+        img="images/gaming.jpg"
+        alt="Texto alternativo para una imagen"
+        title="Un Producto Cualquiera"
+        price="$400">
+        Esto es un prodcuto cualquiera que se puede comprar por react
+      </Item>
+      <h1 className="py-4 text-2xl font-bold text-center">{ greeting || 'Aqui va el catalogo' }</h1>
+    </>
   )
 }
 
