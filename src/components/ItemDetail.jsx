9d228930c8736028d336db46dd4acbbd60f90031
Santiago de Nicolás <santidenicolas@gmail.com>
2022-09-13 10:53:03 -0300

    feat: maquetado del detalle de un producto

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import ItemCount from './ItemCount';
 
-function ItemDetail() {
+function ItemDetail({ img, title, tags, price, description, stock }) {
   return (
-    <div>ItemDetail</div>
+    <div className="grid w-full grid-cols-12 grid-rows-2 gap-2 overflow-hidden">
+      <img src={img} alt="ALT" className="col-start-1 col-end-9 rounded-xl"/>
+      <div className="flex flex-col col-span-11 col-start-9 gap-4 px-5 py-4 border top-5 border-slate-300 rounded-xl h-fit">
+        <h2 className="text-2xl font-bold">{ title }</h2>
+        <div className="flex flex-row flex-wrap gap-3">
+          { tags.map(tag => <span className="px-3 py-1 text-white rounded-lg bg-slate-900">{tag}</span>) }          
+        </div>
+        <span className="text-5xl font-light">${price}</span>
+        <p>{description}</p>
+        <div className='flex w-full gap-4'>
+          <ItemCount className="w-40" stock={stock}/>
+          <button className="flex-1 text-white rounded-lg bg-slate-900">Comprar ahora!</button>
+        </div>
+      </div>
+      {/* <img src="/images/gaming.jpg" alt="ALT" className="flex-1" />
+      <div className="w-32">
+        <h2>Titulo del producto</h2>
+      </div> */}
+    </div>
   )
 }
 
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { useState } from "react"
+import ItemDetail from "./ItemDetail"
 
 function ItemDetailContainer() {
+  const [isLoading, setIsLoading] = useState(true)
+
   return (
-    <div>ItemDetailContainer</div>
+    <div className='px-5 py-6 m-auto max-w-7xl'>
+      {
+        !isLoading ? 
+        <ItemDetail 
+          img='https://www.apple.com/newsroom/images/product/iphone/standard/apple_iphone-12-spring21_purple_04202021_big.jpg.large.jpg' 
+          title='IPhone 12' 
+          tags={['Telefono', 'IPhone', 'Apple']}
+          price="450.000"
+          description="Es un iphone nada más"
+          stock={23}/> 
+        : <p>Cargando...</p>
+      }
+    </div>
   )
 }
 
